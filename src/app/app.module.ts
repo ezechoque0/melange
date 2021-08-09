1fb60e326aca72c4ca1d21db3835d4c57ff5c44d
EZEQUIEL-CHOQUE <ezequielchoque0@gmail.com>
2021-08-09 20:13:52 -0300

    Creo componentes y paginas 😀

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FootComponent } from './components/foot/foot.component';
+import { CardComponent } from './components/card/card.component';
+import { CartComponent } from './components/cart/cart.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FootComponent,
+    CardComponent,
+    CartComponent,
+    InicioComponent,
+    TiendaComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
