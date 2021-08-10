1a48ad634ea441a5482cc43c609c2c4dfc079a71
EZEQUIEL-CHOQUE <ezequielchoque0@gmail.com>
2021-08-09 21:45:09 -0300

    Declaro los pags de nav en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//-------------------------------------------------------------------------------
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component:InicioComponent },
+  { path: 'contacto', component:ContactoComponent },
+  { path: 'nosotros', component:NosotrosComponent },
+  { path: 'tienda', component:TiendaComponent }
+];
+// ---------------------------------NO TOQUE ↓------------------------------------
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
